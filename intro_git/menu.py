1bf214982c56b796f95adfdbf0184999e741f97b
Jonathan Bispo <jokz.bis@gmail.com>
2025-02-27 01:21:59 -0300

    feat: implementa funcao dividir no menu

--- intro_git/menu.py
+++ intro_git/menu.py
@@ -22,4 +22,9 @@ def menu():
         num1 = int(input("Digite o primeiro número da multiplicação: "))
         num2 = int(input("Digite o segundo número da multiplicação: "))
         print("Resultado: " + str(calculadora.multiplicar(num1, num2)))
+        
+    elif escolha == 4:
+        num1 = int(input("Digite o numerador da divisão: "))
+        num2 = int(input("Digite o denominador da divisão: "))
+        print("Resultado: " + str(calculadora.dividir(num1, num2)))
 menu()
