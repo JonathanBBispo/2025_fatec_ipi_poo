83ea2a5bc1aaf959e5cc92253527ae7ec99e12ee
Jonathan Bispo <jokz.bis@gmail.com>
2025-02-27 00:27:40 -0300

    feat: implementa funcao soma no menu

---
+++ intro_git/menu.py
@@ -0,0 +1,16 @@
+import calculadora
+def menu():
+        print("\nEscolha uma opção abaixo:")
+        print("1. Somar")
+        print("2. Subtrair")
+        print("3. Multiplicar")
+        print("4. Dividir")
+        print("0. Sair")
+        escolha = int(input(""))
+        
+        if escolha == 1:
+            num1 = int(input("Insira o primeiro número da soma: "))
+            num2 = int(input("Insira o segundo número da soma: "))
+            print("Resultado: " + str(calculadora.somar(num1, num2)))
+              
+menu()
