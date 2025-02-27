172c9e41973ace71fb4947601bb8af7d149dbc0b
Jonathan Bispo <jokz.bis@gmail.com>
2025-02-27 01:13:42 -0300

    feat: implementa funcao subtrair no menu

--- intro_git/menu.py
+++ intro_git/menu.py
@@ -1,16 +1,20 @@
 import calculadora
 def menu():
-        print("\nEscolha uma opção abaixo:")
-        print("1. Somar")
-        print("2. Subtrair")
-        print("3. Multiplicar")
-        print("4. Dividir")
-        print("0. Sair")
-        escolha = int(input(""))
+    print("\nEscolha uma opção abaixo:")
+    print("1. Somar")
+    print("2. Subtrair")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("0. Sair")
+    escolha = int(input(""))
         
-        if escolha == 1:
-            num1 = int(input("Insira o primeiro número da soma: "))
-            num2 = int(input("Insira o segundo número da soma: "))
-            print("Resultado: " + str(calculadora.somar(num1, num2)))
-              
+    if escolha == 1:
+        num1 = int(input("Insira o primeiro número da soma: "))
+        num2 = int(input("Insira o segundo número da soma: "))
+        print("Resultado: " + str(calculadora.somar(num1, num2)))
+            
+    elif escolha == 2:
+        num1 = int(input("Insira o primeiro número da subtração: "))
+        num2 = int(input("Insira o segundo número da soma: "))
+        print("Resultado: " + str(calculadora.subtrair(num1, num2)))              
 menu()
