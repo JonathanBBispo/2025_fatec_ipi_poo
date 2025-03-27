363351cad79cbb45f553fb77195cc0e800f62022
Aluno <aluno@IPIRANGA.FATEC>
2025-03-27 18:23:56 -0300

    feat(ap006-oo-p2): encerra a apostila

--- oo_parte2/JogoV2.java
+++ oo_parte2/JogoV2.java
@@ -2,27 +2,32 @@ import java.util.Random;
 public class JogoV2 {
     public static void main(String[] args) throws Exception {
         var gerador = new Random();
-        var ng = new Personagem(); 
-        ng.nome = "Curry";
+        var ng = new Personagem();
+        var ng2 = new Personagem("Lebron", 10, 10, 0);
+        var ng3 = new Personagem("Soneca", 4, 4, 10);
+        Personagem [] personagens = {ng, ng2, ng3};
         while (true) {
+            var quemVaiJogar = gerador.nextInt(personagens.length);
             // 1. Caçar
             // 2. Comer
             // 3. Dormir
+            //Descobrir quantos metodos a classe personagem possui
+            // var qtosMetodos = personagem.getMetodos().length; 
             var oQueFazer = 1 + gerador.nextInt(3);
             switch(oQueFazer){
                 case 1:
-                    ng.cacar();
+                    personagens[quemVaiJogar].cacar();
                     break;
                 case 2:
-                    ng.comer();
+                    personagens[quemVaiJogar].comer();
                     break;
                 case 3:
-                    ng.dormir();
+                    personagens[quemVaiJogar].dormir();
                     break;
             }
-        System.out.println(ng);
+        System.out.println(personagens[quemVaiJogar]);
         System.out.println("***********");
-        Thread.sleep(8000);     
+        Thread.sleep(8000);
         }
     }
 }
--- oo_parte2/Personagem.java
+++ oo_parte2/Personagem.java
@@ -1,10 +1,40 @@
+import java.util.Random;
 public class Personagem {
-    String nome = null;
-    int energia = 10;
-    int fome = 0;
-    int sono = 0;
 
-    void cacar(){
+    private String nome = null;
+    private String [] possiveisNomes = {"Steve", "Alex", "Mark"};
+    private int energia;
+    private int fome;
+    private int sono;
+
+    public Personagem(){
+        nome = obterNomeAleatorio();
+        energia = 10;
+        fome = 0;
+        sono = 0;
+    }
+    
+    private String obterNomeAleatorio(){
+        var gerador = new Random();
+        return possiveisNomes[gerador.nextInt(possiveisNomes.length)];
+    }
+    //deve ser possível construir um personagem com dados passados pela clase
+    //caso algum valor seja invalido, usar aquele que é padrão para a respectiva propriedade
+    //incluir a validação de nome: somente valem nomes que tenham pelo menos 4 caracteres
+    //se essa regra for violada, atribuir um nome aleatório
+    public Personagem(String nome, int energia, int fome, int sono){
+        this.nome = nome.length() >= 4 ? nome : obterNomeAleatorio();
+        this.energia = energia >= 0 && energia <= 10 ? energia : 10;        
+        this.fome = fome <= 10 && fome >= 0 ? fome : 0;
+        switch(sono){
+            case 0, 2, 3, 4, 5, 6, 7, 8, 9, 10: 
+                this.sono = sono;
+            default:
+                this.sono = 0;
+        }
+    }
+    
+    public void cacar(){
         if(energia >= 2){
             System.out.println(nome + " caçando");
             energia -=2;    
@@ -15,7 +45,8 @@ public class Personagem {
         fome = fome < 10 ? fome + 1 : fome;
         sono = Math.min(sono + 1,10);
     }
-    void comer(){
+    
+    public void comer(){
         if (fome >= 1){
             System.out.print(nome + " comendo\n");
             fome--;
@@ -25,7 +56,8 @@ public class Personagem {
             System.out.println(nome + " Sem fome para comer");
         }
     }
-    void dormir(){
+    
+    public void dormir(){
         if(sono >= 1){
             System.out.printf("%s dormindo\n", nome);
             sono--;
