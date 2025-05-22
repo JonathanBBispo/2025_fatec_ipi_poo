6841566b0225f879a8d9eaba28e6dd0d5e1dfaf4
Aluno <aluno@IPIRANGA.FATEC>
2025-05-22 18:25:12 -0300

    feat(ap009): faz conexao e menu principal
    - ja temos a fabrica de conexoes
    - ja temos o menu principal
    - estamos usando lombok
    - retomar fazendo case 1, de cadastro

---
+++ banco_de_dados/pessoas/src/main/java/br/mathbbs/ConnectionFactory.java
@@ -0,0 +1,26 @@
+package br.mathbbs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class ConnectionFactory {
+    private String host = "localhost";
+    private String port = "5432";
+    private String db = "20251_fatec_ipi_poo_pessoas_Matheus&Jonathan";
+    private String user = "postgres";
+    private String password = "123456";
+
+    //https://google.com:80/search (protocolo, host, porta, recurso)
+    public Connection obterConexao() throws Exception{
+        //string de conexão
+        var s = String.format(
+            "jdbc:postgresql://%s:%s/%s",
+            host, port, db
+        );
+        //cláusula catch or declare
+        //exceção verificada pelo compilador
+        Connection c = DriverManager.getConnection(
+            s, user, password
+        );
+        return c;
+    }
+}
---
+++ banco_de_dados/script.sql
@@ -0,0 +1,23 @@
+-- DELETE FROM tb_pessoa WHERE cod_pessoa = 2;
+-- SELECT * FROM tb_pessoa ORDER BY nome DESC;
+-- SELECT * FROM tb_pessoa;
+-- UPDATE tb_pessoa SET nome = 'Ana Maria' WHERE cod_pessoa = 1;
+--clausula where
+-- SELECT * FROM tb_pessoa WHERE cod_pessoa IN (1, 3);
+-- SELECT * FROM tb_pessoa WHERE cod_pessoa = 1 OR cod_pessoa = 3;
+-- SELECT * FROM tb_pessoa WHERE cod_pessoa = 1;
+--projecao
+-- SELECT nome, email FROM tb_pessoa;
+-- INSERT INTO tb_pessoa(nome, fone, email)
+-- VALUES
+-- ('Pedro', '78945612', 'pedro@email.com'),
+-- ('Maria', '45612312', 'maria@email.com');
+-- INSERT INTO tb_pessoa(nome, fone, email)
+-- VALUES ('Ana', '12345678', 'ana@email.com');
+-- SELECT * FROM tb_pessoa;
+-- CREATE TABLE tb_pessoa(
+--     cod_pessoa SERIAL PRIMARY KEY,
+--     nome VARCHAR(200) NOT NULL,
+--     fone VARCHAR(200) NOT NULL,
+--     email VARCHAR(200) NULL
+-- );
